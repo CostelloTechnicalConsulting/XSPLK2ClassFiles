9b9240af3bec448c3f120d7f041dfca64bcab04c
Dan Costello <dancos@gmail.com>
2020-01-22 16:35:22 -0500

    Adjustments to ppt and CURL demo

--- Scripts/RESTDemo.cmd
+++ Scripts/RESTDemo.cmd
@@ -1,5 +1,11 @@
 curl -k -u admin:password https://localhost:8089/services/search/jobs/ -d search="search sourcetype=access_* earliest=-7d"
 
+pause
+
 curl -k -u admin:password https://localhost:8089/servicesNS/admin/search/saved/searches/
 
+pause
+
 curl -u admin:password -k https://localhost:8089/servicesNS/admin/search/jobs/1576872034.551/results/ --get -d output_mode=json -d count=5
+
+pause
